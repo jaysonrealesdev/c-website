8f4908ce2a7c8684caa00c3a120dc9d31db57064
Jayson Reales <jaysonfrontend.dev@gmail.com>
2022-11-04 15:34:23 +0800

    commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 AOS.init();
 
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+
 /* || Open Menu On Small Screen */
 const menuBtn = () => {
     const hamburger = document.querySelector('.hamburger');
