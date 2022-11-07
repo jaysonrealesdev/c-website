e86461d369f434281f112e6ba78af195b9d22c98
Jayson Reales <jaysonfrontend.dev@gmail.com>
2022-11-07 15:22:35 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-document.querySelector('#search-input').addEventListener('input', filterList);
+/* document.querySelector('#search-input').addEventListener('input', filterList);
 
 function filterList() {
   const searchInput = document.querySelector('#search-input');
@@ -15,7 +15,30 @@ function filterList() {
       item.style.display = 'none';
     }
   });
-}
+} */
+
+const searchBox_3 = document.getElementById("search-input");
+searchBox_3.addEventListener("keyup",function(){
+  var keyword = this.value;
+  keyword = keyword.toUpperCase();
+  var table_3 = document.getElementById("emp-table");
+  var all_tr = table_3.getElementsByTagName("tr");
+  for(var i=0; i<all_tr.length; i++){
+          var all_columns = all_tr[i].getElementsByTagName("td");
+          for(j=0;j<all_columns.length; j++){
+              if(all_columns[j]){
+                  var column_value = all_columns[j].textContent || all_columns[j].innerText;
+                  column_value = column_value.toUpperCase();
+                  if(column_value.indexOf(keyword) > -1){
+                      all_tr[i].style.display = ""; // show
+                      break;
+                  }else{
+                      all_tr[i].style.display = "none"; // hide
+                  }
+              }
+          }
+      }
+})   
 
 /* || Open Menu On Small Screen */
 const menuBtn = () => {
@@ -58,6 +81,9 @@ const menuBtn = () => {
         }
         }
     }
-
+    function myFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
+    
   menuBtn();
   closeBtn();
