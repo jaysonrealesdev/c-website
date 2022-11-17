d4a4515be859f97502b1c14177e6f22780055c38
Jayson Reales <jaysonfrontend.dev@gmail.com>
2022-11-17 14:22:01 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,64 @@
 AOS.init(); // Iniliaze Animate On Scroll (AOS) Library
 
+let mouseCursor = document.getElementById('cursor');
+let navLink = document.querySelectorAll('.nav-bar li');
+let aLink = document.querySelectorAll('a');
+let aButton = document.querySelectorAll('button');
+
+window.addEventListener("mousemove",cursor);
+
+window.addEventListener("click", () => {
+  mouseCursor.classList.add("expand");
+
+  setTimeout(() => {
+    mouseCursor.classList.remove("expand");
+  }, 500)
+});
+
+function cursor(e) {
+  /* console.log(e); */
+
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX + "px";
+}
+
+aButton.forEach(aButton => {
+  aButton.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("link-grow");
+    mouseCursor.classList.add("hovered-link");
+  });
+
+  aButton .addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("link-grow");
+    mouseCursor.classList.remove("hovered-link");
+  });
+})
+
+aLink.forEach(alink => {
+  alink.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("link-grow");
+    mouseCursor.classList.add("hovered-link");
+  });
+
+  alink .addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("link-grow");
+    mouseCursor.classList.remove("hovered-link");
+  });
+})
+
+navLink.forEach(link => {
+  link.addEventListener("mouseover", () => {
+    mouseCursor.classList.add("link-grow");
+    mouseCursor.classList.add("hovered-link");
+  });
+
+  link.addEventListener("mouseleave", () => {
+    mouseCursor.classList.remove("link-grow");
+    mouseCursor.classList.remove("hovered-link");
+  });
+})
+
+
 const menuBtn = () => { // Open Menu On Small Screen
   const hamburger = document.querySelector('.hamburger');
   hamburger.onclick = function () { 
---
+++ js/typewriter-effect.js
@@ -0,0 +1,6 @@
+const typing = new Typed(".career-text", {
+    strings: ['Solid Career', 'Second Word', 'Third Word'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
