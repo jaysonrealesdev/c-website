d284b5118117d7a9e030ab7e4bdcffc36f536e82
Jayson Reales <jaysonfrontend.dev@gmail.com>
2022-11-22 11:39:42 +0800

    update

---
+++ js/fullscreen-img.js
@@ -0,0 +1,15 @@
+const images = document.querySelectorAll('.img-grid img');
+
+images.forEach(img => {
+    console.log(img);
+    
+});
+
+/* for(i=0; i<images.length; i++) {
+    
+    console.log(images[i]);
+    images[i].addEventListener('click', () => { 
+        this.classList.add("expanded");
+        console.log("images");
+    }); 
+} */
