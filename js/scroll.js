074d19357c1e40ca8da02b0e3169a9357c11a40f
Jayson Reales <jaysonfrontend.dev@gmail.com>
2022-11-23 15:57:27 +0800

    Commiy

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+const scrollBtn = document.getElementById('scroll-btn')
+
+window.onscroll = function()  { scrollFunction() };
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop < 20) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
