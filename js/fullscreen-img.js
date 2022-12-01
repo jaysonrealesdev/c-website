6b24b368514b21e28c90b8797cf00f432627953b
Jayson Reales <jaysonfrontend.dev@gmail.com>
2022-12-01 16:24:36 +0800

    Commit

--- js/fullscreen-img.js
+++ js/fullscreen-img.js
@@ -2,14 +2,7 @@ const images = document.querySelectorAll('.img-grid img');
 
 images.forEach(img => {
     console.log(img);
-    
-});
-
-/* for(i=0; i<images.length; i++) {
-    
-    console.log(images[i]);
-    images[i].addEventListener('click', () => { 
-        this.classList.add("expanded");
+    img.addEventListener('click', () => { 
         console.log("images");
-    }); 
-} */
+    })
+});
--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ const scrollBtn = document.getElementById('scroll-btn')
 window.onscroll = function()  { scrollFunction() };
 
 function scrollFunction() {
-    if(document.body.scrollTop > 20 || document.documentElement.scrollTop < 20) {
+    if(document.body.scrollTop > 300 || document.documentElement.scrollTop < 300) {
         scrollBtn.style.display = "block";
     } else {
         scrollBtn.style.display = "none";
@@ -14,3 +14,18 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add("show")
+        } else {
+            entry.target.classList.remove("show")
+        }
+    })
+})
+
+const hideElements = document.querySelectorAll(".hidden");
+hideElements.forEach((element) => observer.observe(element))
+
